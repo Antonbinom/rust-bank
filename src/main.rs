4cb09d0cc4b70768a6e202b265df279f3c732a7d
Anton Semenikhin <protest_the_hero@mail.ru>
2026-05-20 12:09:06 +0530

    Implement constructors for Bank and Account structs, and add functions to print their details in main.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,36 @@ struct Bank {
     accounts: Vec<Account>,
 }
 
+impl Bank {
+    fn new() -> Self {
+        Bank {
+            accounts: Vec::new(),
+        }
+    }
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            balance: 0,
+            holder,
+        }
+    }
+}
+
+fn print_account(account: Account) {
+    println!("{:#?}", account);
+}
+
+fn print_bank(bank: Bank) {
+    println!("{:#?}", bank);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let bank = Bank::new();
+    let account = Account::new(1, String::from("Anton"));
+
+    print_bank(bank);
+    print_account(account);
 }
